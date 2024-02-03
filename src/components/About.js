06c6dcbda1687e7e547166e210433722a70a7fb1
Jym <126638763+jym0@users.noreply.github.com>
2024-02-03 16:59:10 +0800

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -141,7 +141,7 @@ const About = () => {
             excel in it.
           </p>
 
-          <a className="download_cv" href='/files/Fabellore_Jym_Weil_F_CV.pdf' download="Fabellore_Jym_Weil_F_CV.pdf">
+          <a className="download_cv" href='/files/Fabellore, Jym Weil F._CV.pdf' download="Fabellore, Jym Weil F._CV.pdf">
             <span>download cv</span>
           </a>
         </div>
